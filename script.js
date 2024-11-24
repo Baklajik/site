928463616c2ef7fe19214b0b9e285ce611536953
Baklajik <denisdenis20050315@gmail.com>
2024-11-24 16:47:15 +0300

    7

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Function to open the modal
 function openModal(img) {
     var modal = document.getElementById("myModal");
     var modalImg = document.getElementById("img01");
@@ -9,7 +8,6 @@ function openModal(img) {
     captionText.innerHTML = img.alt;
 }
 
-// Function to close the modal
 function closeModal() {
     var modal = document.getElementById("myModal");
     modal.style.display = "none";
